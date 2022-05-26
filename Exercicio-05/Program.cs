3681ede59ac9049e108a3108af11ec7f1e195705
Myla <Myla@DESKTOP-V9A76BG>
2022-05-26 19:17:27 -0300

    Add project files.

---
+++ Exercicio-05/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int numero1 = 0;
+int numero2 = 0;
+
+
+Console.Write("Digite o primeiro número: ");
+numero1 = Convert.ToInt32(Console.ReadLine());
+
+
+Console.Write("Digite o segundo número: ");
+numero2 = Convert.ToInt32(Console.ReadLine());
+
+if(numero1 > numero2)
+{
+    Console.WriteLine("O número maior é " + numero1);
+}
+else
+{
+    Console.WriteLine("O número maior é " + numero2);
+}
+
